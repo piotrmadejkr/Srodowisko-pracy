fac8d46f703ace0b747a76ffa2fb0b7b0001e0c9
Piotr Madej <piotr.madejkr@gmail.com>
2018-02-28 19:32:34 +0100

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 // scripts.js
 
 var name = prompt('Enter your name');
+// komentarz wylaczajacy alert('Hello, ' + name);
+//komentarz nie wylaczajacy
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+/* koment z gwiazdka*/
